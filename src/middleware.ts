25191ca72554ff6bb828bed7c2f536c361b8870a
Joao V <augustoviegascs@gmail.com>
2026-05-14 11:00:43 -0300

    renamed middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  if (pathname === '/aaa') {
+  if (pathname === '/desativado') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
